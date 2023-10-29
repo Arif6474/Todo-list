bf1a2e9183c3ec2f40c3c4c5085b06f28bef88a9
Arif Islam <arifulislam64743@gmail.com>
2023-10-29 23:27:48 +0600

    todos

---
+++ src/redux/todos/reducer.js
@@ -0,0 +1,70 @@
+/* eslint-disable array-callback-return */
+/* eslint-disable no-unused-expressions */
+import  initialState  from "./initialState";
+import {
+  ADDED,
+  ALLCOMPLETED,
+  CLEARCOMPLETED,
+  COLORSELECTED,
+  DELETED,
+  TOGGLED,
+} from "./actionTypes";
+
+const nextTodoId = (todos) => {
+  const maxId = todos.reduce((maxId, todo) => Math.max(maxId, todo.id), -1);
+  return maxId + 1;
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADDED:
+      return [
+        ...state,
+        {
+          id: nextTodoId(state),
+        },
+      ];
+
+    case TOGGLED:
+      return state.map((todo) => {
+        if (todo.id !== action.payload) {
+          return todo;
+        }
+        return {
+          ...todo,
+          completed: !todo.completed,
+        };
+      });
+
+    case DELETED:
+      return state.filter((todo) => todo.id !== action.payload);
+
+    case COLORSELECTED:
+      const { color, todoId } = action.payload;
+      return state.map((todo) => {
+        if (todo.id !== todoId) {
+          return todo;
+        }
+        return {
+          ...todo,
+          color: color,
+        };
+      });
+
+    case ALLCOMPLETED:
+      return state.map((todo) => {
+        return {
+          ...todo,
+          completed: true,
+        };
+      });
+
+    case CLEARCOMPLETED:
+      return state.filter((todo) => !todo.completed);
+
+    default:
+      return state;
+  }
+};
+
+export default reducer;
